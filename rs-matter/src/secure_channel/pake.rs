b900c01337d526951a981c36a1e1bdd0da9ced93
ivmarkov <ivan.markov@gmail.com>
2024-09-15 15:43:02 +0000

    PASE session timeout was not cleared at the end of the PASE nego

--- rs-matter/src/secure_channel/pake.rs
+++ rs-matter/src/secure_channel/pake.rs
@@ -183,6 +183,8 @@ impl Pake {
         exchange.acknowledge().await?;
         exchange.matter().notify_fabrics_maybe_changed();
 
+        self.clear_timeout(exchange);
+
         Ok(())
     }
 
@@ -360,6 +362,12 @@ impl Pake {
                 Ok(Some(OpCode::PBKDFParamResponse.into()))
             })
             .await
+    }
+
+    fn clear_timeout(&mut self, exchange: &Exchange) {
+        let mut pase = exchange.matter().pase_mgr.borrow_mut();
+
+        pase.timeout = None;
     }
 
     async fn update_timeout(
